f282bf04c7828ee5943fea81b9ecd903f00e2df7
crimson28 <76502656+crimson28@users.noreply.github.com>
2021-04-29 19:58:00 -0500

    Update Wall.js

--- Wall.js
+++ Wall.js
@@ -2,21 +2,27 @@ class Wall {
 
     constructor(x,y,width,height){
         
-        this.x = x;
-        this.y = y;
+        var options = {
+
+            isStatic : true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
         this.height = height;
-        this.color = "white";
-        this.wall;
         
+
+        World.add(world,this.body);
     }
 
 
     display(){
 
        
-       this.wall = createSprite(this.x,this.y,this.width,this.height);
-       this.wall.shapeColor = this.color;
+        var pos = this.body.position;
+       
+       rectMode(CENTER);
+       fill("white");
+       rect(pos.x,pos.y,this.width,this.height);
 
         
     }
